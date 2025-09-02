02f7543da61b7d58eb15203ed636722303ff3c62
Raja Shekar <rajashekardg444@gmail.com>
2025-09-02 07:32:21 +0000

    use this api key : 41308949c66d428c9218a2ae7355fdd3

--- src/services/news.ts
+++ src/services/news.ts
@@ -9,7 +9,7 @@ interface FetchNewsOptions {
   limit?: number;
 }
 
-const GNEWS_API_KEY = "b22ac38c0388be49295828dfa7b73ea0";
+const GNEWS_API_KEY = "41308949c66d428c9218a2ae7355fdd3";
 const NEWSDATA_API_KEY = "pub_3e231d27d02b413a804e6216d1b83058";
 
 
